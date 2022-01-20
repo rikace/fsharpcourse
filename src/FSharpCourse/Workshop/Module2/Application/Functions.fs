7af4b39496fc30fbfc782dd3e65103f921fea076
Kalpana Chowdary Medarametla <v-kameda@microsoft.com>
2022-01-19 16:32:30 -0800

    Module 1 and 2 exercises

--- src/FSharpCourse/Workshop/Module2/Application/Functions.fs
+++ src/FSharpCourse/Workshop/Module2/Application/Functions.fs
@@ -10,3 +10,9 @@ let tryPromoteToVip purchases =
 let getPurchases customer =
     if customer.Id % 2 = 0 then (customer, 120M)
     else (customer, 80M)
+
+let increaseCredit condition customer = 
+   if condition customer then {customer with Credit = customer.Credit + 100M}
+   else {customer with Credit = customer.Credit + 50M}
+
+let increaseCreditusingVip = increaseCredit (fun c->c.IsVip)
--- src/FSharpCourse/Workshop/Module2/Tests/UnitTest1.fs
+++ src/FSharpCourse/Workshop/Module2/Tests/UnitTest1.fs
@@ -10,29 +10,29 @@ let Setup () =
 open Types
 open Functions
 
-//[<Test>]
-//let ``2-1 Increase min credit using id``() =
-//    let customer = { Id = 1; IsVip = false; Credit = 0M }
-//    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
-//    Assert.Equals upgradedCustomer.Credit 50M
-//
-//[<Test>]
-//let ``2-2 Increase max credit using id``() =
-//    let customer = { Id = 2; IsVip = false; Credit = 0M }
-//    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
-//    Assert.Equals upgradedCustomer.Credit 100M
-//
-//[<Test>]
-//let ``2-3 Increase credit keeping existing one``() =
-//    let customer = { Id = 2; IsVip = false; Credit = 10M }
-//    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
-//    Assert.Equals upgradedCustomer.Credit 110M
-//
-//[<Test>]
-//let ``2-4 Increase max credit using vip``() =
-//    let customer = { Id = 2; IsVip = true; Credit = 0M }
-//    let upgradedCustomer = increaseCreditUsingVip customer
-//    Assert.Equals upgradedCustomer.Credit 100M
+[<Test>]
+let ``2-1 Increase min credit using id``() =
+    let customer = { Id = 1; IsVip = false; Credit = 0M }
+    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
+    Assert.IsTrue (upgradedCustomer.Credit = 50M)
+
+[<Test>]
+let ``2-2 Increase max credit using id``() =
+    let customer = { Id = 2; IsVip = false; Credit = 0M }
+    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
+    Assert.IsTrue (upgradedCustomer.Credit = 100M)
+
+[<Test>]
+let ``2-3 Increase credit keeping existing one``() =
+    let customer = { Id = 2; IsVip = false; Credit = 10M }
+    let upgradedCustomer = increaseCredit (fun c -> c.Id = 2) customer
+    Assert.IsTrue (upgradedCustomer.Credit = 110M)
+
+[<Test>]
+let ``2-4 Increase max credit using vip``() =
+    let customer = { Id = 2; IsVip = true; Credit = 0M }
+    let upgradedCustomer = increaseCreditusingVip customer
+    Assert.IsTrue (upgradedCustomer.Credit = 100M)
 //
 //[<Test>]
 //let ``2-5 Upgrade customer with even id``() =
