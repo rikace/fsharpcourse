275bea8800a9b38ce563c01d52b9ec8a6092f88b
Kranti Kishore Degala (HCL TECHNOLOGIES) <v-krade@microsoft.com>
2022-03-04 18:43:09 -0800

    Module 1 Submission

--- src/FSharpCourse/Workshop/Module1/Tests/UnitTest1.fs
+++ src/FSharpCourse/Workshop/Module1/Tests/UnitTest1.fs
@@ -10,31 +10,32 @@ let Setup () =
 open Types
 open Functions
 
+[<Test>]
+let ``1-1 Create customer``() =
+    let customer = { Id = 1; IsVip = false; Credit = 0M }
+    Assert.AreEqual((customer.GetType ()), typeof<Customer>)
 
-//let ``1-1 Create customer``() =
-//    let customer = { Id = 1; IsVip = false; Credit = 0M }
-//    Assert.Equals (customer.GetType ()) typeof<Customer>
 //
-//[<Test>]
-//let ``1-2 Promote to vip``() =
-//    let customer = { Id = 1; IsVip = false; Credit = 0M }
-//    let promotedCustomer = tryPromoteToVip (customer, 100.1M)
-//    Assert.IsTrue promotedCustomer.IsVip
-//
-//[<Test>]
-//let ``1-3 Do not promote to vip``() =
-//    let customer = { Id = 1; IsVip = false; Credit = 0M }
-//    let promotedCustomer = tryPromoteToVip (customer, 99.9M)
-//    Assert.IsFalse promotedCustomer.IsVip
-//
-//[<Test>]
-//let ``1-4 Get purchases for odd customers``() =
-//    let customer = { Id = 1; IsVip = false; Credit = 0M }
-//    let _, purchases = getPurchases customer
-//    Assert.Equals purchases 80M
-//
-//[<Test>]
-//let ``1-5 Get purchases for even customers``() =
-//    let customer = { Id = 2; IsVip = false; Credit = 0M }
-//    let _, purchases = getPurchases customer
-//    Assert.Equals purchases 120M
+[<Test>]
+let ``1-2 Promote to vip``() =
+    let customer = { Id = 2; IsVip = false; Credit = 0M }
+    let promotedCustomer = tryPromoteToVip (customer, 100.1M)
+    Assert.IsTrue promotedCustomer.IsVip
+
+[<Test>]
+let ``1-3 Do not promote to vip``() =
+    let customer = { Id = 1; IsVip = false; Credit = 0M }
+    let promotedCustomer = tryPromoteToVip (customer, 99.9M)
+    Assert.IsFalse promotedCustomer.IsVip
+
+[<Test>]
+let ``1-4 get purchases for odd customers``() =
+    let customer = { Id = 1; IsVip = false; Credit = 0m }
+    let _,purchases = getPurchases customer
+    Assert.AreEqual(purchases,80m)
+
+[<Test>]
+let ``1-5 get purchases for even customers``() =
+    let customer = { Id = 2; IsVip = false; Credit = 0m }
+    let _,purchases = getPurchases customer
+    Assert.AreEqual(purchases,120m)
